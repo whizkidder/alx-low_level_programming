54065c9babea78c9474d37061963209e3eb39ea7
whizkidder <ikehwisdom26@gmail.com>
2022-09-08 17:28:42 -0700

    task 0

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - printing
+ *
+ * Return: Always 0
+  (successful)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzle");
+	return (0);
+}
